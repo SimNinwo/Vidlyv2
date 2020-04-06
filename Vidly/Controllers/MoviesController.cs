bf4c35386d095d76ed4f174c9785c4c9131b6f77
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-04-06 13:58:55 +0100

    finshed an exercse - showing customers and movies available

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -29,5 +29,30 @@ namespace Vidly.Controllers
             return View(viewModel);         
         }
 
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
     }
 }
