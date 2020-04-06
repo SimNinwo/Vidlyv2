f73fd0a036b8009098fc57ae9c3643284f55e359
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-04-06 14:24:35 +0100

    populate MembershipTypes on database using migrations

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,6 +9,10 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
 
     }
 }
