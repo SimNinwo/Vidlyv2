ba324779b0980f76d1e7a683e35053b1bb28b22b
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-04-20 11:20:30 +0100

    Exercise - build API for movies

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(225)]
+        public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+
+    }
+}
